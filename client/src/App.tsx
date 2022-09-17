11b04db8623b9de0fe68f209aa3db56148c8b89d
DennisRono <finnneron1@gmail.com>
2022-09-17 18:10:51 +0300

    ecommerce starter

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import Home from './routes/Home'
+
+const App = () => {
+  return (
+    <Fragment>
+      <Home />
+    </Fragment>
+  )
+}
+
+export default App
