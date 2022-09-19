4ac805be7aae96168fc9e7d4f8c8ed30d16991f1
DennisRono <finnneron1@gmail.com>
2022-09-19 22:20:01 +0300

    surrounded deer iron dull became

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,14 @@
 import React, { Fragment } from 'react'
+import Select from 'react-select'
 import { ReactComponent as Lens} from '../assets/svg/magnifying-glass.svg'
 
 const Header = () => {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+  
   return (
     <Fragment>
       <div className="header">
@@ -10,6 +17,9 @@ const Header = () => {
         </div>
         <div className="header-searcher">
           <form method="post">
+            <div className="header-form-group">
+              <Select options={options} />
+            </div>
             <div className="header-form-group">
               <input type="text" className="h-form-input" />
             </div>
